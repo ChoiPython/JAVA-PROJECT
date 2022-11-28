2c64fda1d4fae0f4b6f20088a96ed6db93b5eff7
skrjsdl03 <skrjsdl03@naver.com>
2022-11-28 17:33:34 +0900

    휴가/반차 사용

---
+++ Vacation.java
@@ -0,0 +1,304 @@
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.*;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+import javax.swing.*;
+
+class CalendarDataManager extends JFrame{
+	static final int CAL_WIDTH = 7;
+	final static int CAL_HEIGHT = 6;
+	int calDates[][] = new int[CAL_HEIGHT][CAL_WIDTH];
+	int calYear;
+	int calMonth;
+	int calDayOfMon;
+	final int calLastDateOfMonth[]={31,28,31,30,31,30,31,31,30,31,30,31};
+	int calLastDate;
+	Calendar today = Calendar.getInstance();
+	Calendar cal;
+	Calendar now;
+	Container c = getContentPane();
+	static int n = 3;
+	
+	public CalendarDataManager(){ 
+		setToday(); 
+	}
+	public void setToday(){
+		calYear = today.get(Calendar.YEAR); 
+		calMonth = today.get(Calendar.MONTH);
+		calDayOfMon = today.get(Calendar.DAY_OF_MONTH);
+		makeCalData(today);
+	}
+	private void makeCalData(Calendar cal){
+		int calStartingPos = (cal.get(Calendar.DAY_OF_WEEK)+7-(cal.get(Calendar.DAY_OF_MONTH))%7)%7;
+		if(calMonth == 1) calLastDate = calLastDateOfMonth[calMonth] + leapCheck(calYear);
+		else calLastDate = calLastDateOfMonth[calMonth];
+		for(int i = 0 ; i<CAL_HEIGHT ; i++){
+			for(int j = 0 ; j<CAL_WIDTH ; j++){
+				calDates[i][j] = 0;
+			}
+		}
+		for(int i = 0, num = 1, k = 0 ; i<CAL_HEIGHT ; i++){
+			if(i == 0) k = calStartingPos;
+			else k = 0;
+			for(int j = k ; j<CAL_WIDTH ; j++){
+				if(num <= calLastDate) calDates[i][j]=num++;
+			}
+		}
+	}
+	private int leapCheck(int year){
+		if(year%4 == 0 && year%100 != 0 || year%400 == 0) return 1;
+		else return 0;
+	}
+	public void moveMonth(int mon){
+		calMonth += mon;
+		if(calMonth>11) while(calMonth>11){
+			calYear++;
+			calMonth -= 12;
+		} else if (calMonth<0) while(calMonth<0){
+			calYear--;
+			calMonth += 12;
+		}
+		cal = new GregorianCalendar(calYear,calMonth,calDayOfMon);
+		makeCalData(cal);
+	}
+}
+
+public class Vacation extends CalendarDataManager{
+	
+	String date = new String();
+	JPanel calOpPanel;
+		JButton todayBut;
+		JLabel todayLab;
+		JButton lYearBut;
+		JButton lMonBut;
+		JLabel curMMYYYYLab;
+		JButton nMonBut;
+		JButton nYearBut;
+		ListenForCalOpButtons lForCalOpButtons = new ListenForCalOpButtons();
+	
+		JButton dateButs[][] = new JButton[6][7];
+		listenForDateButs lForDateButs = new listenForDateButs(); 
+
+	String WEEK_DAY_NAME[] = { "일", "월", "화", "수", "목", "금", "토" };
+
+	public Vacation(){
+		
+		JFrame mainFrame = new JFrame();
+		mainFrame.setTitle("휴가/반차 사용 화면");
+		mainFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		mainFrame.setSize(420,450);
+		mainFrame.setLocationRelativeTo(null);
+		mainFrame.setResizable(false);
+		
+		
+		calOpPanel = new JPanel();
+			lYearBut = new JButton("<<");
+			lYearBut.setToolTipText("Previous Year");
+			lYearBut.addActionListener(lForCalOpButtons);
+			
+			lMonBut = new JButton("<");
+			lMonBut.setToolTipText("Previous Month");
+			lMonBut.addActionListener(lForCalOpButtons);
+			
+			curMMYYYYLab = new JLabel("<html><table width=100><tr><th><font size=5>"+((calMonth+1)<10?"&nbsp;":"")+(calMonth+1)+" / "+calYear+"</th></tr></table></html>");
+		
+			nMonBut = new JButton(">");
+			nMonBut.setToolTipText("Next Month");
+			nMonBut.addActionListener(lForCalOpButtons);
+		
+			nYearBut = new JButton(">>");
+			nYearBut.setToolTipText("Next Year");
+			nYearBut.addActionListener(lForCalOpButtons);
+			
+			calOpPanel.setLayout(new GridBagLayout());
+			GridBagConstraints calOpGC = new GridBagConstraints();
+		
+			calOpGC.anchor = GridBagConstraints.CENTER;
+			calOpGC.gridwidth = 1;
+			calOpGC.gridx = 1;
+			calOpGC.gridy = 2;
+			calOpPanel.add(lYearBut,calOpGC);
+			calOpGC.gridwidth = 1;
+			calOpGC.gridx = 2;
+			calOpGC.gridy = 2;
+			calOpPanel.add(lMonBut,calOpGC);
+			calOpGC.gridwidth = 2;
+			calOpGC.gridx = 3;
+			calOpGC.gridy = 2;
+			calOpPanel.add(curMMYYYYLab,calOpGC);
+			calOpGC.gridwidth = 1;
+			calOpGC.gridx = 5;
+			calOpGC.gridy = 2;
+			calOpPanel.add(nMonBut,calOpGC);
+			calOpGC.gridwidth = 1;
+			calOpGC.gridx = 6;
+			calOpGC.gridy = 2;
+			calOpPanel.add(nYearBut,calOpGC);
+		
+			JPanel calPanel=new JPanel();
+			JButton[] weekDaysName = new JButton[7];
+			for(int i=0 ; i<CAL_WIDTH ; i++){
+				weekDaysName[i]=new JButton(WEEK_DAY_NAME[i]);
+				weekDaysName[i].setBorderPainted(false);
+				weekDaysName[i].setContentAreaFilled(false);
+				weekDaysName[i].setForeground(Color.WHITE);
+				if(i == 0) weekDaysName[i].setBackground(new Color(200, 50, 50));
+				else if (i == 6) weekDaysName[i].setBackground(new Color(50, 100, 200));
+				else weekDaysName[i].setBackground(new Color(150, 150, 150));
+				weekDaysName[i].setOpaque(true);
+				weekDaysName[i].setFocusPainted(false);
+				calPanel.add(weekDaysName[i]);
+			}
+			for(int i=0 ; i<CAL_HEIGHT ; i++){
+				for(int j=0 ; j<CAL_WIDTH ; j++){
+					dateButs[i][j]=new JButton();
+					dateButs[i][j].setBorderPainted(false);
+					dateButs[i][j].setContentAreaFilled(false);
+					dateButs[i][j].setBackground(Color.WHITE);
+					dateButs[i][j].setOpaque(true);
+					dateButs[i][j].addActionListener(lForDateButs);
+//					dateButs[i][j].addActionListener(new ActionListener() {
+//						public void actionPerformed(ActionEvent e) {
+//							if(e.getSource() == dateButs[i][j])
+//							{
+//								JOptionPane.showMessageDialog(null, "남은 인원 " + n + "/3");
+//								date = calYear + "년 " + calMonth + "월 " + calDates + "일";
+//							}
+//						}
+//					});
+					calPanel.add(dateButs[i][j]);
+				}
+			}
+			
+			JPanel frameBottomPanel;
+			JButton AppBtn = new JButton("신청");
+			AppBtn.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent e) {
+					if(e.getSource() == AppBtn)
+					{
+						n--;
+						JOptionPane.showMessageDialog(null, "신청되었습니다.");
+					}
+				}
+			});
+			JLabel bottomInfo = new JLabel(date);
+			
+			calPanel.setLayout(new GridLayout(0,7,2,2));
+			calPanel.setBorder(BorderFactory.createEmptyBorder(0, 5, 0, 5));
+			showCal();
+						
+		JPanel infoPanel = new JPanel();
+						
+		JPanel frameSubPanelWest = new JPanel();
+		Dimension calOpPanelSize = calOpPanel.getPreferredSize();
+		calOpPanelSize.height = 90;
+		calOpPanel.setPreferredSize(calOpPanelSize);
+		frameSubPanelWest.setLayout(new BorderLayout());
+		frameSubPanelWest.add(calOpPanel,BorderLayout.NORTH);
+		frameSubPanelWest.add(calPanel,BorderLayout.CENTER);
+
+		
+		JPanel frameSubPanelEast = new JPanel();
+		Dimension infoPanelSize=infoPanel.getPreferredSize();
+		infoPanelSize.height = 65;
+		infoPanel.setPreferredSize(infoPanelSize);
+		frameSubPanelEast.setLayout(new BorderLayout());
+		frameSubPanelEast.add(infoPanel,BorderLayout.NORTH);
+
+		
+		Dimension frameSubPanelWestSize = frameSubPanelWest.getPreferredSize();
+		frameSubPanelWestSize.width = 410;
+		frameSubPanelWest.setPreferredSize(frameSubPanelWestSize);
+		
+		
+		frameBottomPanel = new JPanel();
+		frameBottomPanel.add(bottomInfo);
+		frameBottomPanel.add(AppBtn);
+		
+
+		mainFrame.setLayout(new BorderLayout());
+		mainFrame.add(frameSubPanelWest, BorderLayout.WEST);
+		mainFrame.add(frameSubPanelEast, BorderLayout.CENTER);
+		mainFrame.add(frameBottomPanel, BorderLayout.SOUTH);
+		mainFrame.setVisible(true);
+
+		focusToday(); 
+	
+	}
+	private void focusToday(){
+		if(today.get(Calendar.DAY_OF_WEEK) == 1)
+			dateButs[today.get(Calendar.WEEK_OF_MONTH)][today.get(Calendar.DAY_OF_WEEK)-1].requestFocusInWindow();
+		else
+			dateButs[today.get(Calendar.WEEK_OF_MONTH)-1][today.get(Calendar.DAY_OF_WEEK)-1].requestFocusInWindow();
+	}
+	private void showCal(){
+		for(int i=0;i<CAL_HEIGHT;i++){
+			for(int j=0;j<CAL_WIDTH;j++){
+				String fontColor="black";
+				if(j==0) fontColor="red";
+				else if(j==6) fontColor="blue";
+				
+				File f =new File("MemoData/"+calYear+((calMonth+1)<10?"0":"")+(calMonth+1)+(calDates[i][j]<10?"0":"")+calDates[i][j]+".txt");
+				if(f.exists()){
+					dateButs[i][j].setText("<html><b><font color="+fontColor+">"+calDates[i][j]+"</font></b></html>");
+				}
+				else dateButs[i][j].setText("<html><font color="+fontColor+">"+calDates[i][j]+"</font></html>");
+
+				JLabel todayMark = new JLabel("<html><font color=green>*</html>");
+				dateButs[i][j].removeAll();
+				if(calMonth == today.get(Calendar.MONTH) &&
+						calYear == today.get(Calendar.YEAR) &&
+						calDates[i][j] == today.get(Calendar.DAY_OF_MONTH)){
+					dateButs[i][j].add(todayMark);
+					dateButs[i][j].setToolTipText("Today");
+				}
+				
+				if(calDates[i][j] == 0) dateButs[i][j].setVisible(false);
+				else dateButs[i][j].setVisible(true);
+			}
+		}
+	}
+	private class ListenForCalOpButtons implements ActionListener{
+		public void actionPerformed(ActionEvent e) {
+			if(e.getSource() == lYearBut) moveMonth(-12);
+			else if(e.getSource() == lMonBut) moveMonth(-1);
+			else if(e.getSource() == nMonBut) moveMonth(1);
+			else if(e.getSource() == nYearBut) moveMonth(12);
+			
+			curMMYYYYLab.setText("<html><table width=100><tr><th><font size=5>"+((calMonth+1)<10?"&nbsp;":"")+(calMonth+1)+" / "+calYear+"</th></tr></table></html>");
+			showCal();
+		}
+	}
+	private class listenForDateButs implements ActionListener{
+		public void actionPerformed(ActionEvent e) {
+			int k=0,l=0;
+			for(int i=0 ; i<CAL_HEIGHT ; i++){
+				for(int j=0 ; j<CAL_WIDTH ; j++){
+					if(e.getSource() == dateButs[i][j]){ 
+						k=i;
+						l=j;
+					}
+				}
+			}
+	
+			if(!(k ==0 && l == 0)) calDayOfMon = calDates[k][l];
+
+			cal = new GregorianCalendar(calYear,calMonth,calDayOfMon);
+			
+			String dDayString = new String();
+			int dDay=((int)((cal.getTimeInMillis() - today.getTimeInMillis())/1000/60/60/24));
+			if(dDay == 0 && (cal.get(Calendar.YEAR) == today.get(Calendar.YEAR)) 
+					&& (cal.get(Calendar.MONTH) == today.get(Calendar.MONTH))
+					&& (cal.get(Calendar.DAY_OF_MONTH) == today.get(Calendar.DAY_OF_MONTH))) dDayString = "Today"; 
+			else if(dDay >=0) dDayString = "D-"+(dDay+1);
+			else if(dDay < 0) dDayString = "D+"+(dDay)*(-1);
+		
+		}
+	}
+	public static void main(String[] args){
+		new Vacation();
+	}
+}
+
