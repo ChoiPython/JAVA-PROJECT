beb89f53d171d1ca7704f555c809bf48da82a1f4
skrjsdl03 <skrjsdl03@naver.com>
2022-12-08 19:09:33 +0900

    신청기능 수정

--- Vacation.java
+++ Vacation.java
@@ -227,32 +227,77 @@ public class Vacation extends CalendarDataManager{
 						System.out.println(user.getId());
 						System.out.println(d);
 						DBA db=new DBA();
-						res=db.leaveApplication(user.getId(), d);
-						if(res==1) {
-							if(Integer.parseInt(day) <= calDayOfMon)	//내일부터 신청가능
-								JOptionPane.showMessageDialog(null, "휴가와 반차신청은 내일부터 신청가능합니다.");
-							else if(Integer.parseInt(day) > calDayOfMon)
-							{
-								if(index == 0)	//휴학/반차 선택
-					            	JOptionPane.showMessageDialog(null, "휴학과 반차중 하나 선택하십시오.");
-					            else if(index == 1)	//휴가 선택
-					            {
+						
+						int todayyear = today.get(Calendar.YEAR);
+						int todaymonth = today.get(Calendar.MONTH)+1;
+						if(todaymonth == 13)
+							todaymonth = 1;
+						int todayday = today.get(Calendar.DAY_OF_MONTH);
+						
+						if(Integer.parseInt(ss[7]) > todayyear)
+						{
+							if(index == 0)	//휴학/반차 선택
+				            	JOptionPane.showMessageDialog(null, "휴학과 반차중 하나 선택하십시오.");
+				            else if(index == 1)	//휴가 선택
+				            {
+				            	res=db.leaveApplication(user.getId(), d,"휴가");
+				            	if(res==1) {
 					            	int h=user.getHalfway()-2;
 									db.updatehalfway(user.getId(), h);
 									JOptionPane.showMessageDialog(null, "신청되었습니다.");
 									n--;
-					            }
-					            else if(index == 2)	//반차 선택
-					            {
+				            	}
+				            	else {
+				            		JOptionPane.showMessageDialog(null, "신청 실패");
+				            	}
+				            }
+				            else if(index == 2)	//반차 선택
+				            {
+				            	res=db.leaveApplication(user.getId(), d,"반차");
+				            	if(res==1) {
 					            	int h=user.getHalfway()-1;
 									db.updatehalfway(user.getId(), h);
 									JOptionPane.showMessageDialog(null, "신청되었습니다.");
 									n--;
-					            }
-							}
+				            	}
+				            	else {
+				            		JOptionPane.showMessageDialog(null, "신청 실패");
+				            	}
+				            }
 						}
-						else 
-							JOptionPane.showMessageDialog(null, "신청 실패");
+						else if(Integer.parseInt(ss[7]) == todayyear && Integer.parseInt(ss[5]) >= todaymonth && Integer.parseInt(day) > todayday)
+						{
+							if(index == 0)	//휴학/반차 선택
+				            	JOptionPane.showMessageDialog(null, "휴학과 반차중 하나 선택하십시오.");
+				            else if(index == 1)	//휴가 선택
+				            {
+				            	res=db.leaveApplication(user.getId(), d,"휴가");
+				            	if(res==1) {
+					            	int h=user.getHalfway()-2;
+									db.updatehalfway(user.getId(), h);
+									JOptionPane.showMessageDialog(null, "신청되었습니다.");
+									n--;
+				            	}
+				            	else {
+				            		JOptionPane.showMessageDialog(null, "신청 실패");
+				            	}
+				            }
+				            else if(index == 2)	//반차 선택
+				            {
+				            	res=db.leaveApplication(user.getId(), d,"반차");
+				            	if(res==1) {
+					            	int h=user.getHalfway()-1;
+									db.updatehalfway(user.getId(), h);
+									JOptionPane.showMessageDialog(null, "신청되었습니다.");
+									n--;
+				            	}
+				            	else {
+				            		JOptionPane.showMessageDialog(null, "신청 실패");
+				            	}
+				            }
+						}
+						else
+							JOptionPane.showMessageDialog(null, "휴가와 반차신청은 내일부터 신청가능합니다.");
 					}
 				}
 			});
